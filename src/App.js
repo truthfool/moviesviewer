ad4c982ee89961da24e0f24c1e0bb7ea4944f567
shani <ishanranasingh@gmail.com>
2022-04-03 00:31:49 +0530

    movie card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieCard from './components/MovieCard';
 
 function App() {
+  
+  const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=f54ed28e'
+  const movie1={
+    "Title": "Batman v Superman: Dawn of Justice",
+    "Year": "2016",
+    "imdbID": "tt2975590",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+  const [moviename,setMovieName] = useState(movie1)
+
+
   return (
     <div className="App">
-      
+      <div className="container">
+          <h2>Movies Viewer</h2>
+          <input 
+          placeholder="Search movie name"
+          onClick={()=>{}}
+          >
+          </input>
+          <button onClick={()=>{}}>Search</button>
+      </div>
+      <div className="movies-list">
+        <MovieCard moviename={moviename}/>
+      </div>
     </div>
   );
 }
