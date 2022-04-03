ab89bc7c8916b4d9491c54035c358378479a0ee5
Ayushmina-20 <ayushminanayak20@gmail.com>
2022-04-04 00:23:09 +0530

    my-changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import MovieCard from './components/MovieCard';
+import './App.css'
 
 function App() {
   
@@ -16,15 +17,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-          <h2>Movies Viewer</h2>
-          <input 
+      <div className="Home">
+        <div className='heading'> <h2>Movies Viewer</h2></div>
+         
+          <div className='Searchbar'> <input 
           value={searchTerm}
           onChange={(e)=>{setSearchTerm(e.target.value)}}
           placeholder="Search movie name"
           >
           </input>
-          <button onClick={()=>{searchMovieName(searchTerm)}}>Search</button>
+          <button onClick={()=>{searchMovieName(searchTerm)}}>Search</button></div>
+         
       </div>
       <div className="movies-list">
         {movienames.map((movie)=>(
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React from "react";
 import Card from "react-bootstrap/Card";
 
-function MovieCard({movie}) {
-  console.log(movie)  
+function MovieCard({ movie }) {
+  console.log(movie);
   return (
-    <div className="card">
-        <Card style={{ width: '18rem', height:'5rem' }}>
-            <Card.Img variant="top" src={movie.Poster!=='N/A'?movie.Poster:"https://via.placeholder.com/400"} alt={"No Image"} />
-            <Card.Body>
-                <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text>
-                Year : {movie.imdbID}<br/>
-                Type : {movie.Type}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </div>
-  )
+    <>
+      <Card
+        style={{
+          width: "23%",
+          height: "350px",
+          marginRight: "22px",
+          marginBottom: "15px",
+        }}
+      >
+        <Card.Img
+          style={{
+            width: "98%",
+            height: "180px",
+            margin: "auto",
+            marginTop: "5px",
+          }}
+          variant="top"
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={"No Image"}
+        />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>
+            Year : {movie.imdbID}
+            <br />
+            Type : {movie.Type}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
