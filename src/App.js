2bbf691800a9a0c5b3e77439ac3f124069807a4d
shani <ishanranasingh@gmail.com>
2022-04-04 00:17:16 +0530

    correction in css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { useState } from 'react';
-import MovieCard from './components/MovieCard';
+import Home from './components/Home';
+import './App.css'
 
 function App() {
-  
-  const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=f54ed28e'
-  const [movienames,setMovieNames] = useState([])
-  const [searchTerm,setSearchTerm]=useState("")
-
-  const searchMovieName=async (searchTerm)=>{
-      const response=await fetch(`${API_URL}&s=${searchTerm}`)
-      const val = await response.json()
-      // console.log(val)
-      setMovieNames(val.Search)
-  };
-
   return (
     <div className="App">
-      <div className="container">
-          <h2>Movies Viewer</h2>
-          <input 
-          value={searchTerm}
-          onChange={(e)=>{setSearchTerm(e.target.value)}}
-          placeholder="Search movie name"
-          >
-          </input>
-          <button onClick={()=>{searchMovieName(searchTerm)}}>Search</button>
-      </div>
-      <div className="movies-list">
-        {movienames.map((movie)=>(
-          <MovieCard movie={movie}/>
-        ))}
-      </div>
+      <Home />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Card from "react-bootstrap/Card";
+import './styles/moviecard.css'
 
 function MovieCard({movie}) {
   console.log(movie)  
   return (
-    <div className="card">
-        <Card style={{ width: '18rem', height:'5rem' }}>
-            <Card.Img variant="top" src={movie.Poster!=='N/A'?movie.Poster:"https://via.placeholder.com/400"} alt={"No Image"} />
+    <>
+        <Card className="movie-card">
+            <Card.Img className="card-image" variant="top" src={movie.Poster!=='N/A'?movie.Poster:"https://via.placeholder.com/400"} alt={"No Image"} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>
-                Year : {movie.imdbID}<br/>
-                Type : {movie.Type}
+                  Year : {movie.Year}<br/>
+                  Type : {movie.Type}
                 </Card.Text>
             </Card.Body>
         </Card>
-    </div>
+    </>
   )
 }
 
